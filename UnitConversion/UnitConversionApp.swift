e9358b030785f75e54d5a45a72b3b6151dd324e9
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-01-11 12:10:58 +0530

    inital commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Dhurv Dugar on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
